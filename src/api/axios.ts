d86e27110c4fb2470a008b00be3cd1480b5f3e44
진수화 <shusbox@naver.com>
2026-06-23 20:25:12 +0900

    add: baseURL 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://',
+  baseURL: 'https://anyone-enhance-lustrous.ngrok-free.dev/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
