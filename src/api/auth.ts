bbf5ce10b6023a501b644a063aab4c68b8512627
진수화 <shusbox@naver.com>
2026-06-24 10:24:21 +0900

    add: login API 함수 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,24 @@
 import api from './axios';
 
+/**
+ * 회원가입 API 함수
+ * @param username 이름
+ * @param email 이메일
+ * @param password 비밀번호
+ * @returns response.data
+ */
 export const signup = async (username: string, email: string, password: string) => {
   const response = await api.post('/users', { username, email, password });
+  return response.data;
+};
+
+/**
+ * 로그인 API 함수
+ * @param email 이메일
+ * @param password 비밀번호
+ * @returns response.data
+ */
+export const login = async (email: string, password: string) => {
+  const response = await api.post('/auth/login', { email, password });
   return response.data;
 };
