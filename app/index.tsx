afeda482a78330398d34088a92139f21b98998ba
진수화 <shusbox@naver.com>
2026-06-25 20:36:41 +0900

    add: 랜딩페이지 Toast 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useRouter, useLocalSearchParams } from 'expo-router';
 import { Pressable, View } from 'react-native';
-import { useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Stack, Button, Text } from '@/components';
+import { Stack, Button, Text, Toast } from '@/components';
 
 export default function App() {
   const router = useRouter();
+  const { toast } = useLocalSearchParams();
+  const [showToast, setShowToast] = useState(false);
+
+  useEffect(() => {
+    if (toast === "success") {
+      setShowToast(true);
+    }
+  }, [toast]);
 
   return (
     <View className="flex-1 pb-10 justify-end">
       <StatusBar style="auto" />
       <Stack gap="xxl" className="items-center">
+        {showToast && <Toast text="회원가입이 완료되었습니다." onClose={() => setShowToast(false)} />}
         <Button variant="disabled" icon="mail" onPress={() => router.push('/Login')}> 이메일로 로그인하기 </Button>
         <Pressable onPress={() => router.push("/Signup")}><Text weight="medium" color="disabled"> 회원가입 </Text></Pressable>
       </Stack>
