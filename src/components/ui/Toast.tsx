4dfe53005064bac2ab94940e21cdea7d02805d89
진수화 <shusbox@naver.com>
2026-06-25 20:36:26 +0900

    add: Toast 컴포넌트 애니메이션 추가

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { View } from "react-native";
 import { Text } from "./Text";
+import Animated, { FadeOut } from "react-native-reanimated";
 
 // ================================
 // Components
@@ -7,17 +9,29 @@ import { Text } from "./Text";
 
 interface Props {
   text: string;
+  onClose: () => void;
   className?: string;
 }
 
 /**
  * Toast 컴포넌트
  * @param text Toast 컴포넌트에 들어갈 Text를 입력합니다.
+ * @param onClose Toast의 Timeout이 종료될 때 실행할 행동을 입력합니다.
  */
-export function Toast({ text, className = "" }: Props) {
+export function Toast({ text, onClose, className = "" }: Props) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
   return (
-    <View className="absolute bottom-[68px] w-full items-center z-10">
-      <View
+    <Animated.View
+      exiting={FadeOut.duration(300)}
+      className="absolute bottom-[80px] w-full items-center z-10"
+    >
+      <Animated.View
         className={`
           py-m px-l
           rounded-xs
@@ -25,8 +39,8 @@ export function Toast({ text, className = "" }: Props) {
           ${className}
         `}
       >
-        <Text variant="base-medium"> {text} </Text>
-      </View>
-    </View>
+        <Text variant="base-medium">{text}</Text>
+      </Animated.View>
+    </Animated.View>
   );
 }
