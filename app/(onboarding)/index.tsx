b3e511c59485ff68c5a53c09e03ae012ea3f9005
진수화 <shusbox@naver.com>
2026-07-14 13:50:08 +0900

    fix: auth 경로 수정

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -20,8 +20,8 @@ export default function App() {
       <StatusBar style="auto" />
       <Stack gap="xxl" className="items-center">
         {showToast && <Toast text="회원가입이 완료되었습니다." onClose={() => setShowToast(false)} />}
-        <Button variant="primary" icon="mail" onPress={() => router.push('/Login')}> 이메일로 로그인하기 </Button>
-        <Pressable onPress={() => router.push("/Signup")}><Text weight="medium" color="disabled"> 회원가입 </Text></Pressable>
+        <Button variant="primary" icon="mail" onPress={() => router.push('/auth/Login')}> 이메일로 로그인하기 </Button>
+        <Pressable onPress={() => router.push("/auth/Signup")}><Text weight="medium" color="disabled"> 회원가입 </Text></Pressable>
       </Stack>
     </View>
   );
