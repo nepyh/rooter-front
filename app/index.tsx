d1e99ec37accca87b0901ed1f511f450074284ae
ㅠㅠ무뭄 <taehug662@gmail.com>
2026-07-30 03:48:08 +0900

    add: 시작 화면에 마스코트 로고와 Rooter 타이틀 추가

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { useRouter, useLocalSearchParams } from 'expo-router';
 import { Pressable, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Stack, Button, Text, Toast } from '@/components';
+import { Icon } from '@/assets';
 
 export default function App() {
   const router = useRouter();
@@ -18,6 +19,12 @@ export default function App() {
   return (
     <View className="flex-1 pb-10 justify-end">
       <StatusBar style="auto" />
+      <Stack gap="l" className="flex-1 items-center justify-center">
+        <Icon name="mascot" size={140} color="#0ED9FD" />
+        <Text weight="semibold" style={{ color: "#0ED9FD", fontSize: 50 }}>
+          Rooter
+        </Text>
+      </Stack>
       <Stack gap="xxl" className="items-center">
         {showToast && <Toast text="회원가입이 완료되었습니다." onClose={() => setShowToast(false)} />}
         <Button variant="primary" icon="mail" onPress={() => router.push('/Login')}> 이메일로 로그인하기 </Button>
